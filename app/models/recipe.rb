c6f9736d3f62a555dc9ba4e690816f6c9770890e
Sapsford11 <craig.sapsford11@gmail.com>
2016-04-27 13:16:30 +0200

    Updated image size

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -13,6 +13,6 @@ class Recipe < ActiveRecord::Base
 
   validates :title, :description, :image, presence: true
 
-	has_attached_file :image, styles: { :medium => "500x454#" }
+	has_attached_file :image, styles: { :medium => "500x500#" }
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
